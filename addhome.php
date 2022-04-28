f7940394d3b66cc4eea898e5f3c8b353a8d1bdae
Benny Lisangi <bennylisangi@Bennys-MacBook-Air.local>
2022-04-28 13:47:19 -0400

    can read houses from the database in the homepage

--- addhome.php
+++ addhome.php
@@ -18,8 +18,8 @@ if(is_null($_SESSION["firstname"])){
             <input type="number" name="zip" placeholder="30045" required/>
             <input type="text" name="country" placeholder="country" required/>
             <input type="number" name="price" placeholder="$2,000,000" required/>
-            <input type="file" name="IMG" placeholder="An image of the house" required/>
-            <input type="file" name="floor" placeholder="An image of a floor in the house" required/>
+            <input type="file" name="IMG" placeholder="An image of the house" required accept=".jpeg, .png, .gif"/>
+            <input type="file" name="floor" placeholder="An image of a floor in the house" required accept=".png, .gif, .jpeg"/>
             <input type="submit" value="Sell this house"/>
 
         </form>
--- home.php
+++ home.php
@@ -17,7 +17,21 @@ if(is_null($_SESSION["firstname"])){
         <a href="addhome.php" target="_blank"> Sell your house here.</a>
     </div>
         <?php
-        echo "<h1>Welcome home".$_SESSION["firstname"]."</h1>"
+        include 'connect.php';
+        echo "<h1>Welcome home ".$_SESSION["firstname"]."!</h1>";
+        $sql = "SELECT * FROM Homes WHERE sold = 0";
+        $results = $conn->query($sql);
+        if($results->num_rows>0){
+            while($row = mysqli_fetch_assoc($results)){
+                echo "<div id = 'listing'>";
+                echo $row["street"].", ".$row["city"].", ".$row["state"]." ".$row["zip"]."<br/>";
+                echo "Listed for: ".$row["price"];
+                echo "</div>";
+            }
+        }
+        else{
+            echo "No listing available.";
+        }
         ?>
     </body>
 </html>
